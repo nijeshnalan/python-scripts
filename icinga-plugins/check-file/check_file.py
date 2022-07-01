f8a27920c03e279c29107f8bc2bb33ea23f146db
nijeshnalan <niju4all@gmail.com>
2022-07-01 22:51:45 +0530

    A simple NRPE plugin to check a file exists or not

---
+++ icinga-plugins/check-file/check_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from os.path import exists
+import sys
+
+arguments = sys.argv[1:]
+
+failCount = 0
+
+for argument in arguments:
+
+    if exists(argument):
+        print("OK - File {} exists".format(argument))
+
+    else:
+        print("CRITICAL - File {} not exists".format(argument))
+        fileCounter += 1
+
+if failCount == 0:
+    sys.exit(0)
+
+else:
+    sys.exit(2)
